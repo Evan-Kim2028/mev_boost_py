2d70119d5d048dfd80585f29965fc9658a39e536
Evan-Kim2028 <ekcopersonal@gmail.com>
2024-09-02 16:59:35 -0400

    feat: add docs and explanation to test

--- test/proposer_payload.py
+++ test/proposer_payload.py
@@ -1,7 +1,11 @@
-from mev_boost_py import ProposerPayloadFetcher
+from mev_boost_py.proposer_payload import ProposerPayloadFetcher
 
 # Create a fetcher instance
-fetcher = ProposerPayloadFetcher(start_slot=2447969, end_slot=2448969)
+fetcher = ProposerPayloadFetcher(
+    # Use the start_slot and end_slot arguments to specify a range of slots to fetch. If none, it will default to the most recent 200 slots.
+    # start_slot=2447969, 
+    # end_slot=2448969
+    )
 
 # Run the fetcher
 fetcher.run()
